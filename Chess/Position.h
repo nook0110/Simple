cec28a8d7a97c064951f10e5be3a1e09e64c2011
Arseniy <lazo07032005@gmail.com>
2023-08-28 00:31:58 +0300

    King position updating

--- Chess/Position.h
+++ Chess/Position.h
@@ -52,11 +52,6 @@ class Position
    */
   void PlacePiece(const BitIndex square, const Piece piece, const Player color)
   {
-    if (piece == Piece::kKing)
-    {
-      king_positions_[static_cast<size_t>(color)] = square;
-    }
-
     assert(!board_[square]);
     assert(piece);
     board_[square] = piece;
@@ -256,7 +251,7 @@ class Position
 
   std::array<Piece, kBoardArea> board_{};  //!< Current position of pieces
 
-  std::array<BitIndex, kColors> king_positions_{};
+  std::array<BitIndex, kColors> king_position_{};
 
   std::array<std::array<Bitboard, 2>, kColors> castling_squares_for_king_{};
   std::array<std::array<Bitboard, 2>, kColors> castling_squares_for_rook_{};
@@ -313,7 +308,7 @@ inline Piece Position::GetPiece(const BitIndex index) const
 
 inline BitIndex Position::GetKingSquare(const Player player) const
 {
-  return king_positions_[static_cast<size_t>(player)];
+  return king_position_[static_cast<size_t>(player)];
 }
 
 inline bool Position::IsUnderAttack(const BitIndex square,
