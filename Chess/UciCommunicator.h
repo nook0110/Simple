52afe0d4da98b64e962969a92f6ac1b8c8b4cd66
nook0110 <nik.blokhtin@gmail.com>
2024-01-01 21:58:15 +0300

    fix

--- Chess/UciCommunicator.h
+++ Chess/UciCommunicator.h
@@ -48,7 +48,8 @@ class UciDebugPrinter final : public InfoPrinter
   void operator()(
       const PrincipalVariationInfo& principal_variation) const override
   {
-    o_stream_ << "info pv";
+    o_stream_ << "info depth " << principal_variation.best_moves.size() + 1
+              << "info pv";
     for (const auto move : principal_variation.best_moves)
     {
       o_stream_ << " ";
