4638c0adcd4a5a1ad3dd6b5df46cbf1b7a2aeb9b
nook0110 <nik.blokhtin@gmail.com>
2024-05-29 13:49:48 +0300

    test added

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -269,8 +269,7 @@ TEST(DoMove, DoAndUndoEqualZero)
 
 TEST(SomeMoves, DifferentHash)
 {
-  const auto start_pos = PositionFactory{}();
-  Position pos = start_pos;
+  auto pos = PositionFactory{}();
 
   std::vector<std::string> moves = {"b1c3", "d7d5", "a1b1"};
   std::set<Hash> hashes = {pos.GetHash()};
@@ -280,6 +279,32 @@ TEST(SomeMoves, DifferentHash)
     pos.DoMove(move);
     ASSERT_TRUE(hashes.insert(pos.GetHash()).second);
   }
+}
+
+Position DoMoves(Position pos, const std::vector<std::string>& moves)
+{
+  for (const auto& move_str : moves)
+  {
+    auto move = MoveFactory{}(pos, move_str);
+
+    pos.DoMove(move);
+  }
+  return pos;
+}
+
+TEST(TwoSimiliarPositions, DifferentHash)
+{
+  const auto start_pos = PositionFactory{}();
+
+  std::vector<std::string> first_moves_str = {"e2e4", "e7e5", "d2d4", "c7c5",
+                                              "d4e5"};
+  std::vector<std::string> second_moves_str = {"e2e4", "e7e5", "d2d4", "c7c5",
+                                               "d4c5"};
+
+  const auto first_position = DoMoves(start_pos, first_moves_str);
+  const auto second_position = DoMoves(start_pos, second_moves_str);
+
+  ASSERT_NE(first_position.GetHash(), second_position.GetHash());
 }
 }  // namespace PositionTest
 
