39d8007d70ed6442a018df6ae4ddf68f1ce6fcb2
nook0110 <nik.blokhtin@gmail.com>
2024-06-17 15:05:34 +0300

    parse command line

--- Chess/main.cpp
+++ Chess/main.cpp
@@ -1,11 +1,22 @@
 #include "UciCommunicator.h"
 
-int main() {
+int main(int argc, char** argv) {
   SimpleChessEngine::InitBetween<SimpleChessEngine::Piece::kBishop>();
   SimpleChessEngine::InitBetween<SimpleChessEngine::Piece::kRook>();
   SimpleChessEngine::InitPawnAttacks();
   SimpleChessEngine::InitPSQT();
-
-  SimpleChessEngine::UciChessEngine uci;
+  if (argc == 1) {
+    SimpleChessEngine::UciChessEngine uci;
+    uci.Start();
+    return EXIT_SUCCESS;
+  }
+  std::stringstream ss;
+  for (size_t arg_idx = 1; arg_idx < argc; ++arg_idx) {
+    ss << argv[arg_idx] << "\n";
+  }
+  SimpleChessEngine::UciChessEngine uci(ss);
   uci.Start();
+  return EXIT_SUCCESS;
 }
+//"position fen r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq
+//-" "go perft 6" "stop"
